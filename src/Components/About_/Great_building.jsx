3e39698125f0aad030594025025da30942f5bdde
AnasAhmedFND <anasahmed4402@gmail.com>
2025-06-02 12:20:57 +0600

    great_building section design done

---
+++ src/Components/About_/Great_building.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { FaArrowRight } from "react-icons/fa6";
+
+const Great_building = () => {
+  return (
+    <section className='container mx-auto py-5  '>
+        <p className='text-center text-red-500'>Great Experience in building</p>
+        <div className="flex justify-between mt-10">
+            <div className="w-[30%] border border-dashed border-black rounded-b-[50px] py-2 text-center shadow-xl shadow-[#c6adcd] ">
+                <img className='mx-auto' src="/about/bl1.png" alt="" />
+                <h5 className='font-bold '>Best Repair & <br />painting </h5>
+                <p>There are many variations of passages of Lorem,…</p>
+
+                <button className='border bg-black text-white  w-[50%] py-2 rounded-t-full flex justify-center items-center mx-auto mt-2 '><FaArrowRight /></button>
+                
+            </div>
+
+             <div className="w-[30%] border border-dashed border-black rounded-b-[50px] py-2 text-center shadow-xl shadow-[#c6adcd] ">
+                <img className='mx-auto' src="/about/bl2.png" alt="" />
+                <h5 className='font-bold '>Heavy <br /> Equipment </h5>
+                <p>There are many variations of passages of Lorem,…</p>
+
+                <button className='border bg-black text-white  w-[50%] py-2 rounded-t-full flex justify-center items-center mx-auto mt-2 '><FaArrowRight /></button>
+                
+            </div>
+
+             <div className="w-[30%] border border-dashed border-black rounded-b-[50px] py-2 text-center shadow-xl shadow-[#c6adcd] ">
+                <img className='mx-auto' src="/about/bl3.png" alt="" />
+                <h5 className='font-bold '>Apartment <br /> Design </h5>
+                <p>There are many variations of passages of Lorem,…</p>
+
+                <button className='border bg-black text-white  w-[50%] py-2 rounded-t-full flex justify-center items-center mx-auto mt-2 '><FaArrowRight /></button>
+                
+            </div>
+
+        </div>
+
+        <div className="mt-[60px] flex border text-[12px] mx-auto gap-[22px] justify-center w-[42%] items-center pl-2 rounded-r-full ">
+        <p className='text-red-500'>Offeromg High Quality Construction Solutions</p>
+        <button className='border bg-[#ff6600] px-4 text-white py-2 rounded-r-full'>Build Your Dream Now </button>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Great_building
--- src/app/abouts/page.jsx
+++ src/app/abouts/page.jsx
@@ -1,5 +1,6 @@
 import About_servics from '@/Components/About_/About_servics'
 import Contractors from '@/Components/About_/Contractors'
+import Great_building from '@/Components/About_/Great_building'
 import Hero from '@/Components/hero_/Hero'
 import React from 'react'
 
@@ -9,6 +10,7 @@ const page = () => {
        <Hero page_title={"About"} page={'Blog'} />
        <Contractors />
        <About_servics />
+       <Great_building />
        
     </div>
   )
